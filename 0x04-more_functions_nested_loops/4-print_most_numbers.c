76933ab547898a7cf7e632b02ef6c0229c876a09
Gerry-Aballa <gerry.o.aballa@gmail.com>
2022-07-07 22:05:33 +0000

    Upadated 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,13 +13,7 @@ void print_most_numbers(void)
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		if (i != 2 && i != 4)
-		{
-			_putchar(i);
-		}
-		else
-		{
-			continue;
-		}
+		_putchar(i % 10);
 	}
 	_putchar('\n');
 }
