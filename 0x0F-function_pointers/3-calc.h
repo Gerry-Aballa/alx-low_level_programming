055bd77bfde42acc234b2a26e4f9ac91008952bc
Gerry-Aballa <gerry.o.aballa@gmail.com>
2022-08-03 22:24:11 +0000

    Added 4 files

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,18 +1,5 @@
-#ifndef TASK3
-#define TASK3
-
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-char *op;
-int (*f)(int a, int b);
-} op_t;
-
+#ifndef _CALC_H_
+#define _CALC_H_
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -20,4 +7,15 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+**/
+typedef struct op
+{
+char *op;
+int (*f)(int a, int b);
+} op_t;
 #endif
